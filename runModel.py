f0353de2e05cc2e897b2f489a21aba9b6557b942
asianonice <idrummer97@gmail.com>
2018-03-30 20:40:45 -0700

    Finshed script to be run by application.

--- runModel.py
+++ runModel.py
@@ -1,16 +1,12 @@
-from __future__ import print_function
 import csv
-import numpy as np
-import tensorflow as tf
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
 import pickle
-
 
 # Get features and labels from csv file
 
 # Organize data to be used for running
 
+# just a test:
+X = [[75,85,94,100,65,50,80,40,75,95,90,80,80,80,90,100,99,85,75,65,70,70,50,40,60,60,80,100,20,30,100,50,70,100,70,100,75,60,40,70]]
 
 #Running Models:
 
@@ -21,7 +17,16 @@ loaded_cohModel = pickle.load(open('finalized_cohModel.sav', 'rb'))
 
 #X_test and Y_test are the data from the app
 cohPrediction = loaded_cohModel.predict(X)
+
 print(cohPrediction)
+
+# Classifications for Change of Habits:
+# 1 == Consistent Motivation
+# 2 == Finding Peace
+# 3 == Being Involved in Community
+# 4 == Health Habits
+# 5 == Balancing Time with Electronics and Life
+# 6 == Money Management
 
 # (2) Run model on Small Group data:
 
@@ -33,6 +38,12 @@ SmallGroupPrediction = loaded_SmallGroupModel.predict(X)
 
 print(SmallGroupPrediction)
 
+# Classifications for Small Group:
+# 1 == Leader
+# 2 == Prayer Warrior
+# 3 == Normal Member
+# 4 == Relational Leader
+
 # (3) Run model on Serve Group data:
 
 # load the model from disk
@@ -43,6 +54,13 @@ ServeGroupPrediction = loaded_ServeGroupModel.predict(X)
 
 print(ServeGroupPrediction)
 
+# Classifications for Serve Group:
+# 1 == Administration
+# 2 == Hospitality
+# 3 == Outreach
+# 4 == Evangelism
+# 5 == Teaching
+
 # (4) Run model on Positive Qualities data:
 
 # load the model from disk
@@ -52,5 +70,13 @@ loaded_pqModel = pickle.load(open('finalized_pqModel.sav', 'rb'))
 pqPrediction = loaded_pqModel.predict(X)
 
 print(pqPrediction)
+
+# Classifications for Positive Qualities:
+# 1 == Gentleness
+# 2 == Wisdom
+# 3 == Faithuful Servant
+# 4 == Recognizing Faults and Encourage Change
+# 5 == Evangelism
+# 6 == Good Life Decisions
 
 # return cohPrediction, SmallGroupPrediction, ServeGroupPrediction, pqPrediction
