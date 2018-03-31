6c80d6056ae59565384f77b1c66fdfbc26f93eff
asianonice <idrummer97@gmail.com>
2018-03-30 19:44:02 -0700

    Made scripts work for our use cases. (CSV files without SF and MB)

--- trainPositiveQualities.py
+++ trainPositiveQualities.py
@@ -1,46 +1,50 @@
 from __future__ import print_function
-
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
-import pandas as pd
+from sklearn.model_selection import train_test_split
 import pickle
 
-# Ignore all GPUs, tf random forest does not benefit from it.
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+fileName = 'SurveyPositiveQualities.csv'
 
-# Get features and labels from csv file
+# Get data from csv for reading
+fullCSV= np.array(list(csv.reader(open(fileName, "r"))))
+
+colCount = fullCSV.shape[1]
 
 # Organize data to be used for training
 
-    # Create lists for Serving Group Model
+# Changing the type from string to int.
+fullCSV = fullCSV.astype(np.int)
+
+# Split data into labels (Y) and Features (X)
+Y = fullCSV[:, 0]
+X = fullCSV[:, 1:colCount]
 
 # Train and test Models
 
-    # (1) Ensemble with Random Forests (sklearn)
+# (1) Ensemble with Random Forests (sklearn)
 
-    # Random Forest Parameters
+# Random Forest Parameters
 
-    # Train test splitting data set
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3 ,random_state=0)
+# Train test splitting data set
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3 ,random_state=0)
 
-    # Model fitting
-    forest = RandomForestClassifier(n_estimators=500)
-    forest.fit(X_train,y_train)
+# Model fitting
+forest = RandomForestClassifier(n_estimators=5000)
+forest.fit(X_train,y_train)
 
-    # Test Random Forest
-    # Model score
-    print(forest.score(X_test,y_test))
+# Test Random Forest
+# Model score
+print( 'The Score is: ')
+print(forest.score(X_test,y_test))
 
 
-    # save the model to disk
-    filename = 'finalized_pqModel.sav'
-    pickle.dump(model, open(filename, 'wb'))
+# save the model to disk
+filename = 'finalized_pqModel.sav'
+pickle.dump(model, open(filename, 'wb'))
 
 # Basic Artificial Neural Network
 
